08fac76bca3211e5407e0b6a36a99c10c485cbb7
tangyibo123 <867461411@qq.com>
2022-06-14 18:55:52 +0800

    add 3d tag

---
+++ framework/src/main/java/com/tangyibo/framework/tag/TagAdapter.java
@@ -0,0 +1,27 @@
+package com.tangyibo.framework.tag;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class TagAdapter {
+    private OnDataSetChangeListener onDataSetChangeListener;
+
+    public abstract int getCount();
+    public abstract View getView(Context context, int position, ViewGroup parent);
+    public abstract Object getItem(int position);
+    public abstract int getPopularity(int position);
+    public abstract void onThemeColorChanged(View view,int themeColor);
+
+    public final void notifyDataSetChanged() {
+        onDataSetChangeListener.onChange();
+    }
+
+    protected interface OnDataSetChangeListener{
+        void onChange();
+    }
+
+    protected void setOnDataSetChangeListener(OnDataSetChangeListener listener) {
+        onDataSetChangeListener = listener;
+    }
+}
